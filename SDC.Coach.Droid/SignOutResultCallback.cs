8358925e1a0a56e6ba05fe0677d911759cf43ff2
Roman Rudyak <roma.rudyak@gmail.com>
2018-06-21 16:54:00 +0300

    integrated sample of android google sign-in

---
+++ SDC.Coach.Droid/SignOutResultCallback.cs
@@ -0,0 +1,15 @@
+﻿using Java.Lang;
+using Android.Gms.Common.Apis;
+
+namespace SDC.Coach.Droid
+{
+    public class SignOutResultCallback : Object, IResultCallback
+    {
+        public MainActivity Activity { get; set; }
+
+        public void OnResult(Object result)
+        {
+            Activity.UpdateUI(false);
+        }
+    }
+}
