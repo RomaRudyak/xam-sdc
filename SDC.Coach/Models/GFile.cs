4ad07b38ce944c175fd8978f7968cecb5da38525
Roman Rudyak <roma.rudyak@gmail.com>
2018-07-22 23:03:41 +0300

    Add Google drive rest api declaration

---
+++ SDC.Coach/Models/GFile.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SDC.Coach.Models
+{
+    public class GFile
+    {
+        public String Id { get; set; }
+        public String Name { get; set; }
+        public String MimeType { get; set; }
+    }
+}
