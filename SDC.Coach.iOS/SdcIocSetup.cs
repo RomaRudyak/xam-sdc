6d34035181c93f833c0d8b2554fdb66319b70603
Roman Rudyak <roma.rudyak@gmail.com>
2018-07-23 12:52:14 +0300

    configured ioc to Autofac for ios Project

---
+++ SDC.Coach.iOS/SdcIocSetup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Autofac;
+using MvvmCross.IoC;
+using MvvmCross.Platforms.Ios.Core;
+using MvvmCross.ViewModels;
+using SDC.Coach.IoC;
+
+namespace SDC.Coach.iOS
+{
+    public class SdcIocSetup<TApp> : MvxIosSetup<TApp>
+        where TApp : class, IMvxApplication, new()
+    {
+        protected override IMvxIoCProvider CreateIocProvider()
+        {
+            return new AutofacMvxIocProvider(new ContainerBuilder().Build());
+        }
+    }
+}
