65d1d4241d8db95776c0343955c220005c021079
Roman Rudyak <roma.rudyak@gmail.com>
2018-07-23 13:31:16 +0300

    add infrastructure for Autofac type registrations

---
+++ SDC.Coach/IoC/SdcIoCInitializer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Autofac;
+
+namespace SDC.Coach.IoC
+{
+    public class SdcIoCInitializer : AppIoCInitializer
+    {
+        public override void Initialize(ContainerBuilder containerBuilder)
+        {
+            // Note: place here container specific initialization 
+            // dependent on Autofact specific features.
+            // To override or extend IoC with platform specifics types
+            // inherit from SdcIoCInitializer on platform project
+            // override Initialize add specific types after base.Initialize
+            // shanged initialization type in plaform specific MvxSetup
+        }
+    }
+}
