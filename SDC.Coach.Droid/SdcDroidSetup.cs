5f27d6935a97e83597a7a4aa51fbbb0ac0484bc7
Roman Rudyak <roma.rudyak@gmail.com>
2018-07-23 12:47:14 +0300

    Setup Autofac container for Droid

--- SDC.Coach.Droid/SdcDroidSetup.cs
+++ SDC.Coach.Droid/SdcDroidSetup.cs
@@ -1,11 +1,17 @@
 ﻿using MvvmCross.Droid.Support.V7.AppCompat;
+using MvvmCross.IoC;
 using MvvmCross.ViewModels;
+using SDC.Coach.IoC;
+using Autofac;
 
 namespace SDC.Coach.Droid
 {
     public class SdcDroidSetup<TAapp> : MvxAppCompatSetup<TAapp>
         where TAapp : class, IMvxApplication, new()
     {
-
+        protected override IMvxIoCProvider CreateIocProvider()
+        {
+            return new AutofacMvxIocProvider(new ContainerBuilder().Build());
+        }
     }
 }
