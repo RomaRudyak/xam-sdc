caa763ee641029d86283752f213da108d8c3427f
Roman Rudyak <roma.rudyak@gmail.com>
2018-09-01 16:25:53 +0300

    add Groups table view

---
+++ SDC.Coach.iOS/VIews/GroupsView.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using UIKit;
+using MvvmCross.Platforms.Ios.Views;
+using SDC.Coach.ViewModels;
+
+namespace SDC.Coach.iOS.VIews
+{
+    public partial class GroupsView : MvxTableViewController<GroupsViewModel>
+    {
+        public GroupsView() : base(UITableViewStyle.Plain)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
+
